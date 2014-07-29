a0a204ddff07250eb83036e6d0ad979e9b6302d0
wlljk2002 <wlljk2002@gmail.com>
2014-07-29 17:33:06 +0800

    1st commit. UI framework.

---
+++ src/com/techbow/liulunchdelivery/FragmentList.java
@@ -0,0 +1,19 @@
+package com.techbow.liulunchdelivery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentList extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View rootView = inflater.inflate(R.layout.fragment_list, container, false);
+		return rootView;
+	}
+
+}
---
+++ src/com/techbow/liulunchdelivery/PagerAdapterListMap.java
@@ -0,0 +1,47 @@
+package com.techbow.liulunchdelivery;
+
+import java.util.Locale;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PagerAdapterListMap extends FragmentPagerAdapter {
+	private Context context;
+	public PagerAdapterListMap(FragmentManager fm, Context context) {
+		super(fm);
+		// TODO Auto-generated constructor stub
+		this.context = context;
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		// TODO Auto-generated method stub
+		switch (position) {
+		case 0:
+			return new FragmentList();
+		case 1:
+			return new FragmentMap();
+		}
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 2;	//list and map
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		Locale l = Locale.getDefault();
+		switch (position) {
+		case 0:
+			return context.getString(R.string.title_section1).toUpperCase(l);
+		case 1:
+			return context.getString(R.string.title_section2).toUpperCase(l);
+		}
+		return null;
+	}
+}
