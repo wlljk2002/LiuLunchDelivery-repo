44114ac1834549db28a6cd40448e9989abe3aaf1
wlljk2002 <wlljk2002@163.com>
2014-08-31 14:58:07 +0800

    transplant is done, parse + google map is working now

--- src/com/techbow/liulunchdelivery/listmap/DistributionListMapAsyncTask.java
+++ src/com/techbow/liulunchdelivery/listmap/DistributionListMapAsyncTask.java
@@ -2,17 +2,15 @@ package com.techbow.liulunchdelivery.listmap;
 
 import java.util.List;
 
-import com.baidu.mapapi.map.BaiduMap.OnMarkerClickListener;
-import com.baidu.mapapi.map.BitmapDescriptor;
-import com.baidu.mapapi.map.BitmapDescriptorFactory;
-import com.baidu.mapapi.map.MapStatus;
-import com.baidu.mapapi.map.MapStatusUpdate;
-import com.baidu.mapapi.map.MapStatusUpdateFactory;
-import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.Marker;
-import com.baidu.mapapi.map.MarkerOptions;
-import com.baidu.mapapi.map.OverlayOptions;
-import com.baidu.mapapi.model.LatLng;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
@@ -61,12 +59,12 @@ public class DistributionListMapAsyncTask extends AsyncTask<Void, Void, Void> {
 		distributionGeoList.clear();
 		distributionListViewAdapter.notifyDataSetChanged();
 		
-		// first we need to get mapview reference from context, complicated...
+		// first we need to get MapView reference from context, complicated...
 		ActivityListMap activity = (ActivityListMap) context;
 		ViewPager pager = (ViewPager)activity.findViewById(R.id.listMapPager);
 		FragmentPagerAdapter f = (FragmentPagerAdapter) pager.getAdapter();
-		FragmentMap fragmentMap = (FragmentMap)f.instantiateItem(pager, 1);
-		mapView = fragmentMap.mMapView;
+		FragmentGoogleMap fragmentMap = (FragmentGoogleMap)f.instantiateItem(pager, 1);
+		mapView = fragmentMap.mapView;
 		mapView.getMap().clear();
 		dialog = fragmentMap.dialog;
 		dialog.changeStatusWord("Loading near lunch distibution sites, please wait...");
@@ -74,7 +72,7 @@ public class DistributionListMapAsyncTask extends AsyncTask<Void, Void, Void> {
 	@Override
 	protected Void doInBackground(Void... arg0) {
 		// TODO Auto-generated method stub
-		ParseGeoPoint curLoc = new ParseGeoPoint(FragmentMap.locationData.latitude, FragmentMap.locationData.longitude);
+		ParseGeoPoint curLoc = new ParseGeoPoint(FragmentGoogleMap.curPosition.latitude, FragmentGoogleMap.curPosition.longitude);
 		ParseQuery<ParseObject> queryGeo = new ParseQuery<ParseObject>("DistributionGeo");
 		queryGeo.whereNear("point", curLoc);
 		queryGeo.setLimit(6); //获取最接近用户地点的6条数据
@@ -126,44 +124,39 @@ public class DistributionListMapAsyncTask extends AsyncTask<Void, Void, Void> {
 		distributionListViewAdapter.notifyDataSetChanged();
 		
 		LatLng latLng = null;
-        OverlayOptions overlayOptions = null;
         Marker marker = null;
         //构建Marker图标  
         BitmapDescriptor markerIcon = BitmapDescriptorFactory.fromResource(R.drawable.maker);
         for (int i = 0; i < distributionGeoList.size(); i++) {
 			// 位置  
             latLng = new LatLng(distributionGeoList.get(i).getPoint().getLatitude(), distributionGeoList.get(i).getPoint().getLongitude());  
-            // 图标  
-            overlayOptions = new MarkerOptions().position(latLng).icon(markerIcon).zIndex(5);  
-            marker = (Marker) (mapView.getMap().addOverlay(overlayOptions));  
-            Bundle bundle = new Bundle();
-            bundle.putSerializable("distributionSite", distributionSiteList.get(i));
-            bundle.putString("distributionSiteObjectId", distributionGeoList.get(i).getDistributionSiteObjectId());
-            marker.setExtraInfo(bundle);
-		}
+            // 图标
+            marker = mapView.getMap().addMarker(new MarkerOptions()
+	            .position(latLng)
+	            .snippet(String.valueOf(i))
+	            .icon(markerIcon));
+        }
         //对Marker的点击
         mapView.getMap().setOnMarkerClickListener(new OnMarkerClickListener()
   		{
   			@Override
-  			public boolean onMarkerClick(final Marker marker)
+  			public boolean onMarkerClick(Marker marker)
   			{
   				//获得marker中的数据
-  				DistributionSite site = (DistributionSite) marker.getExtraInfo().getSerializable("distributionSite");
+  				int idx = Integer.valueOf(marker.getSnippet()).intValue();
+  				DistributionSite site = distributionSiteList.get(idx);
 				//Toast.makeText(context, "site " + site.getName() + " is chosen", Toast.LENGTH_SHORT).show();
 				Intent intent = new Intent(context, ActivityLunch.class);
 				intent.putExtra("distributionSite", site);
-				intent.putExtra("distributionSiteObjectId", marker.getExtraInfo().getString("distributionSiteObjectId"));
+				intent.putExtra("distributionSiteObjectId", distributionGeoList.get(idx).getDistributionSiteObjectId());
 				context.startActivity(intent);
   				return true;
   			}
   		});
         LatLng point = new LatLng(distributionGeoList.get(0).getPoint().getLatitude(), distributionGeoList.get(0).getPoint().getLongitude());
-		
-		// 基于原来的status上改变指定的参数
-		MapStatus mapStatus = new MapStatus.Builder(mapView.getMap().getMapStatus()).target(point).build();
-		MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mapStatus);
-		mapView.getMap().animateMapStatus(mapStatusUpdate); // 改变,这里设置成随着移动而改变位置
-		
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(point, 16); //zoom level 16
+        mapView.getMap().moveCamera(update);
+        
 		dialog.hide();
 	}
 }
--- src/com/techbow/liulunchdelivery/listmap/FragmentGoogleMap.java
+++ src/com/techbow/liulunchdelivery/listmap/FragmentGoogleMap.java
@@ -1,20 +1,32 @@
 package com.techbow.liulunchdelivery.listmap;
 
+import org.apache.http.util.LangUtils;
+
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.baidu.location.BDLocation;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MyLocationData;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
 import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
 import com.parse.ParseGeoPoint;
 import com.techbow.liulunchdelivery.R;
 import com.techbow.liulunchdelivery.Utils.LoadingAndWaitDialog;
@@ -26,14 +38,18 @@ public class FragmentGoogleMap extends Fragment
 	ConnectionCallbacks,
 	OnConnectionFailedListener,
 	LocationListener {
-	private MapView mapView;
+	public MapView mapView;
 	private GoogleMap map;
 	private LocationClient locationClient;
+	private boolean isFirstLocate = true;
+	public LoadingAndWaitDialog dialog;
+	public static LatLng curPosition;
+	
     // These settings are the same as the settings for the map. They will in fact give you updates
     // at the maximal rates currently possible.
     private static final LocationRequest REQUEST = LocationRequest.create()
-            .setInterval(5000)         // 5 seconds
-            .setFastestInterval(16)    // 16ms = 60fps
+            .setInterval(60000)         // 5 seconds
+            .setFastestInterval(5000)    // 16ms = 60fps
             .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     
 	@Override
@@ -46,11 +62,17 @@ public class FragmentGoogleMap extends Fragment
 		map = mapView.getMap();
 		if (map != null) {
             map.setMyLocationEnabled(true);
+            map.setBuildingsEnabled(true);
+            MapsInitializer.initialize(getActivity());
         }
 		locationClient = new LocationClient(
                 getActivity().getApplicationContext(),
                 this,  // ConnectionCallbacks
                 this); // OnConnectionFailedListener
+		
+		dialog = new LoadingAndWaitDialog(getActivity());
+		dialog.changeStatusWord("Locating your position, please wait...");
+		dialog.show();
 		return rootView;
 	}
 	@Override
@@ -107,8 +129,29 @@ public class FragmentGoogleMap extends Fragment
 	}
 
 	@Override
-	public void onLocationChanged(Location arg0) {
+	public void onLocationChanged(Location location) {
 		// TODO Auto-generated method stub
-		
+		if (location == null)
+            return ;
+		StringBuffer sb = new StringBuffer(256);
+		sb.append("time : ");
+		sb.append(location.getTime());
+		sb.append("\nlatitude : ");
+		sb.append(location.getLatitude()); // 纬度
+		sb.append("\nlontitude : ");
+		sb.append(location.getLongitude()); // 经度
+		sb.append("\naltitude : ");
+		sb.append(location.getAltitude()); // 高度
+		sb.append("\nspeed : ");
+		sb.append(location.getSpeed()); // 速度
+		Log.w("google location", sb.toString());
+		if(isFirstLocate) {
+			FragmentList.asyncTask.execute();
+			isFirstLocate = false;
+			
+			curPosition = new LatLng(location.getLatitude(), location.getLongitude());
+			CameraUpdate update = CameraUpdateFactory.newLatLngZoom(curPosition, 16); //zoom level 16
+			map.moveCamera(update);
+		}
 	}
 }
